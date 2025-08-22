b3eebb0bdd5c5cf8242a5f8fa0b66268bebeacad
Chema <jimbomilk@hotmail.com>
2025-08-22 20:45:37 +0200

    .init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from backend.config import Config
+from config import Config
 
 # Inicializamos las extensiones
 db = SQLAlchemy()
